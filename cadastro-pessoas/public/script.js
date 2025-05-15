9765e244685f2b5264e95b5a5223af91cfab11c8
luanlel <luan.l.silva6@ba.estudante.senai.br>
2025-05-15 10:48:10 -0300

    cadastro-pessoas

---
+++ cadastro-pessoas/public/script.js
@@ -0,0 +1,35 @@
+// public/script.js
+
+// Ao enviar o formulário
+document.getElementById("form").addEventListener("submit", async function(e) {
+    e.preventDefault();
+    const nome = document.getElementById("nome").value;
+
+    // Enviar o nome para o servidor
+    await fetch("/cadastrar", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ nome })
+    });
+
+    document.getElementById("nome").value = "";
+    carregarPessoas();
+
+ });
+
+ // Carrega a lista de pessoas
+    async function carregarPessoas() {
+        const resposta = await fetch("/listar");
+        const pessoas = await resposta.json();
+
+        const lista = document.getElementById("lista");
+        lista.innerHTML = ""; // Limpa a lista atual
+        pessoas.forEach(pessoa => {
+            const li = document.createElement("li");
+            li.textContent = pessoa.nome;
+            lista.appendChild(li);
+        });
+
+}
