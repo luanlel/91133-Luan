a71b5eeb74418c58744c10f5be53b3281e2491ee
luanlel <luan.l.silva6@ba.estudante.senai.br>
2025-09-18 12:25:51 -0300

    Update Ex11 - LimpLista.test.js

--- Mercadinho - Testes/Ex11 - LimpLista.test.js
+++ Mercadinho - Testes/Ex11 - LimpLista.test.js
@@ -3,7 +3,7 @@
 const { Produto, Mercadinho } = require('./Mercadinho');
 
 describe('Limpeza da lista', () => {
-  it('Deve esvaziar a lista após chamar limpar()', () => {
+  it('Após adicionar produtos e chamar limpar(), a lista deve estar vazia.', () => {
     // cenário
     const mercado = new Mercadinho();
     mercado.adicionarProduto(new Produto(9, 'Farinha', 7, 9));
